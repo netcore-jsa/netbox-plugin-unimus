abb807ad63aae2817c5cfa9d2d1c39686f976281
Twooey <michael@thewireless.ninja>
2021-01-07 17:40:10 -0500

    Still not working as intended. Something with the build is messing up adding the plugin.

---
+++ netbox_unimus/__init__.py
@@ -0,0 +1,17 @@
+from extras.plugins import PluginConfig
+
+
+class Unimus(PluginConfig):
+    name = "netbox_unimus"
+    verbose_name = "Unimus Integration"
+    description = "Integrates Netbox and Unimus"
+    version = "0.1"
+    author = "Michael Rhone (michael@rhone.dev)"
+    author_email = "michael@rhone.dev"
+    base_url = "Unimus"
+    min_version = "2.9"
+    required_settings = []
+    default_settings = {}
+
+
+config = Unimus
