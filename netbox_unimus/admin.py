a271ad79846dadb12493a385f2fe52252fb1d152
Twooey <michael@rhone.dev>
2021-01-25 14:54:29 -0500

    Minor, but important change.
    From the django documentation, this seems like the django way of doing what we need.
    
    This should let me do proper verification to see if the api is working.

--- netbox_unimus/admin.py
+++ netbox_unimus/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
+from django import forms
 from .model import UnimusIntegration
 
 
-@admin.register(UnimusIntegration)
-class BgpPeeringAdmin(admin.ModelAdmin):
+class UnimusForm(forms.ModelForm):
+    class Meta:
+        model = UnimusIntegration
+        fields = '__all__'
+
+class UnimusAdmin(admin.ModelAdmin):
+    form = UnimusForm
     list_display = ('Unimus_Address',
                     'API_Token',
                     'Verify_Certificate',
                     'Test')
+
+admin.site.register(UnimusIntegration, UnimusAdmin)
--- netbox_unimus/model.py
+++ netbox_unimus/model.py
@@ -2,11 +2,9 @@ from django.core.validators import RegexValidator
 from django.db import models
 from extras.models import ChangeLoggedModel
 
-class UnimusIntegration(ChangeLoggedModel):
+class UnimusIntegration(models.Model):
     Unimus_Address = models.URLField()
     API_Token = models.TextField(validators=[RegexValidator(regex='^.{101}$', message='Length has to be 101', code='nomatch')])
     Verify_Certificate = models.BooleanField(default=False)
     Test = models.BooleanField(default=True)
 
-    def clean_task_for(self):
-        data = self.clean_task_for('task_for')
