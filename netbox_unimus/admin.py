3b807ede77db3c1ea70259b50359ba07535a7ffb
Twooey <michael@rhone.dev>
2021-02-04 11:28:31 -0500

    We have clean data figured out. We can start moving rapidly now on validation.
    I have the framework set up for a basic Python Unimus API, I need to include the basic features we need for this plugin, import it, and use it for validation.

--- netbox_unimus/admin.py
+++ netbox_unimus/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django import forms
 from .model import UnimusIntegration
+from django.core.exceptions import ValidationError
 
 
 class UnimusForm(forms.ModelForm):
@@ -8,11 +9,19 @@ class UnimusForm(forms.ModelForm):
         model = UnimusIntegration
         fields = '__all__'
 
+    def clean_Test(self):
+        data = self.cleaned_data['Test']
+        if not data:
+            raise ValidationError("This is broken on purpose!")
+        return data
+
+
 class UnimusAdmin(admin.ModelAdmin):
     form = UnimusForm
     list_display = ('Unimus_Address',
                     'API_Token',
                     'Verify_Certificate',
                     'Test')
+
 
 admin.site.register(UnimusIntegration, UnimusAdmin)
