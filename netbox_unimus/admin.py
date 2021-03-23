46d570d1fee4a819c42f21fdb64147297382f0d8
Twooey <michael@rhone.dev>
2021-03-23 18:24:18 -0400

    We can now verify SSL certs.
    Testing that the unifi server address is correct is still on hold until we publish the python-unimus package.

--- netbox_unimus/admin.py
+++ netbox_unimus/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django import forms
 from .model import UnimusIntegration
 from django.core.exceptions import ValidationError
+import requests
 
 
 class UnimusForm(forms.ModelForm):
@@ -9,12 +10,25 @@ class UnimusForm(forms.ModelForm):
         model = UnimusIntegration
         fields = '__all__'
 
+    # This will be implimented when the python-unimus client is published.
+
     def clean_Test(self):
         data = self.cleaned_data['Test']
         if not data:
             raise ValidationError("This is broken on purpose!")
         return data
 
+
+    def clean_Verify_Certificate(self):
+
+        data_Verify_Certificate = self.cleaned_data['Verify_Certificate']
+        if data_Verify_Certificate:
+            data_url = self.cleaned_data['Unimus_Address']
+            try:
+                requests.get(data_url)
+            except requests.exceptions.SSLError:
+                raise ValidationError("Not a valid SSL Cert")
+        return data_Verify_Certificate
 
 class UnimusAdmin(admin.ModelAdmin):
     form = UnimusForm
